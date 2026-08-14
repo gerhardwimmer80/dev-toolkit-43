cccd8229c77bfdad0023fd6d31b51c7baa1b280d
Matthias Roth <gerhardwimmer80@gmail.com>
2026-08-14 06:59:20 +0300

    add logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,40 +1,17 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-class Logger:
-    def __init__(self, name: str):
-        self.logger = logging.getLogger(name)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
-        self.logger.setLevel(logging.DEBUG)
+def setup_logger(name, log_file, level=logging.INFO):
+    handler = RotatingFileHandler(log_file, maxBytes=5*1024*1024, backupCount=3)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    return logger
 
-    def debug(self, message: str):
-        self.logger.debug(message)
-
-    def info(self, message: str):
-        self.logger.info(message)
-
-    def warning(self, message: str):
-        self.logger.warning(message)
-
-    def error(self, message: str):
-        self.logger.error(message)
-
-    def critical(self, message: str):
-        self.logger.critical(message)
-
-# Create a default logger instance for usage
-default_logger = Logger('default_logger')
-def log_message(message: str, level: str = 'info'):
-    level = level.lower()
-    if level == 'debug':
-        default_logger.debug(message)
-    elif level == 'warning':
-        default_logger.warning(message)
-    elif level == 'error':
-        default_logger.error(message)
-    elif level == 'critical':
-        default_logger.critical(message)
-    else:
-        default_logger.info(message)
+if __name__ == '__main__':
+    logger = setup_logger('example_logger', 'example.log')
+    logger.info('This is an info message')
+    logger.error('This is an error message')
+    logger.debug('This is a debug message')
