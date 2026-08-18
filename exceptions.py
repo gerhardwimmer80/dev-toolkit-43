36fa1c4527ad4d1f0e48327b22b8a5c949cf5574
Matthias Roth <gerhardwimmer80@gmail.com>
2026-08-19 00:15:27 +0300

    update custom error classes for exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,33 +1,24 @@
 class CustomError(Exception):
-    """Base class for all custom exceptions"""
-    def __init__(self, message):
+    pass
+
+class NotFoundError(CustomError):
+    def __init__(self, message="Resource not found"):
         self.message = message
         super().__init__(self.message)
 
 class ValidationError(CustomError):
-    """Raised when a validation check fails"""
-    def __init__(self, field, message):
+    def __init__(self, field, message="Invalid value"):
         self.field = field
-        self.message = f'Validation error on {field}: {message}'
+        self.message = f"{field}: {message}"
         super().__init__(self.message)
 
-class NotFoundError(CustomError):
-    """Raised when a requested resource is not found"""
-    def __init__(self, resource_id):
-        self.resource_id = resource_id
-        self.message = f'Resource with ID {resource_id} not found'
+class DatabaseConnectionError(CustomError):
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.message = f"Cannot connect to database: {db_name}"
         super().__init__(self.message)
 
-class PermissionError(CustomError):
-    """Raised when access is denied"""
-    def __init__(self, resource):
-        self.resource = resource
-        self.message = f'Access denied to resource: {resource}'
-        super().__init__(self.message)
-
-class ConfigurationError(CustomError):
-    """Raised when there is a configuration issue"""
-    def __init__(self, config_key):
-        self.config_key = config_key
-        self.message = f'Configuration error with key: {config_key}'
+class UnauthorizedAccessError(CustomError):
+    def __init__(self):
+        self.message = "You do not have permission to access this resource"
         super().__init__(self.message)
